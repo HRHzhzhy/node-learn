153b08f0bb8f36f52276cf71887989e580e9ef5a
张振勇 <zhenyongzhang@163.com>
2017-09-05 21:58:36 +0800

    数据处理demo

--- 6demo注册登录.js
+++ 6demo注册登录.js
@@ -7,8 +7,8 @@ const http = require('http')
 const fs = require('fs')
 const querystring = require('querystring')
 const urlLib = require('url')
-
-var server = http.createServer((req, res) => {
+var users = {} // 存储用户名和密码 {'zhangsan': '123', 'lisi', '1234'}
+var server = http.createServer(function (req, res) {
   // 解析数据
   var str = ''
   req.on('data', (data) => {
@@ -18,8 +18,10 @@ var server = http.createServer((req, res) => {
   req.on('end', () => {
     // get解析
     var obj = urlLib.parse(req.url, true)
-    const url = obj.pathname
+    const url = decodeURIComponent(obj.pathname)
     const GET = obj.query
+    console.log(obj.query)
+    console.log(url)
     // post解析
     const POST = querystring.parse(str)
     // 对数据进行处理，区分接口和文件
@@ -27,19 +29,46 @@ var server = http.createServer((req, res) => {
       switch(GET.act){
         case 'reg':
         // 检查用户，插入用户
-        break
-        // 检查用户，检查用户密码
+        for (let key in users) {
+          console.log(`${key}: ${users[key]}`)
+        }
+          if (users[GET.name]) {
+            
+            res.write('{"ok": false, "msg": "用户已存在"}')
+          } else {
+            users[GET.name] = GET.pass
+            for (let key in users) {
+              console.log(`${key}: ${users[key]}`)
+            }
+            res.write('{"ok": true, "msg": "注册成功"}')
+          }
+        break;
+        
         case 'login':
+        // 检查用户，检查用户密码
+        for (let key in users) {
+          console.log(`${key}: ${users[key]}`)
+        }
+        console.log(users[GET.name])
+          if (!users[GET.name]) {
+            res.write('{"ok": false, "msg": "用户不存在"}')
+          } else {
+            if (users[GET.name] === GET.pass){
+              res.write('{"ok": true, "msg": "登录成功"}')
+            } else {
+              res.write('{"ok": false, "msg": "密码错误"}')
+            }
+          }
         break
         default:
         res.write('{"ok": false, "msg": "act非法"}')
-        res.end()
       }
+      res.end()
     } else { // 处理文件
       var fileName = `./www${url}`
       fs.readFile(fileName, (err, data) => {
         if (err) {
-          res.write('{"ok": false, "msg": "文件不存在"}')
+          res.write('{"ok": false, "msg": "404"}')
         } else {
           res.write(data)
         }
@@ -47,4 +76,4 @@ var server = http.createServer((req, res) => {
       })
     }
   })
-})
+}).listen('8888')
